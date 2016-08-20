5eaa2a4810da3c27ecf067f683dd0dbe0c9c0a69
Segmond <segmond@gmail.com>
2016-08-20 00:42:59 -0400

    work in progress

--- sql/1_ksms.sql
+++ sql/1_ksms.sql
@@ -5,14 +5,27 @@ CREATE TABLE ksms.person (
 	name character varying NOT NULL,
 	street character varying NOT NULL,
 	town character varying NOT NULL,
-	phone numeric(10,0)
+	phone numeric(10,0),
+	email_address character varying NOT NULL
+);
+
+CREATE TABLE ksms.belt (
+	id serial PRIMARY KEY,
+	description character varying NOT NULL
 );
 
 CREATE TABLE ksms.student (
-	person_id bigserial PRIMARY KEY,
+	person_id bigserial PRIMARY KEY REFERENCES ksms.person (id),
 	enroll_date timestamp with time zone NOT NULL,
 	drop_date timestamp with time zone NOT NULL,
- 	FOREIGN KEY (person_id) REFERENCES ksms.person (id)
+	grading_date timestamp with time zone,
+	belt_id integer REFERENCES ksms.belt (id)
+);
+
+CREATE TABLE ksms.membership_hold (
+	student_id bigserial REFERENCES ksms.student (person_id),
+	start_date timestamp with time zone NOT NULL,
+	end_ate timestamp with time zone NOT NULL
 );
 
 CREATE TABLE ksms.payment_type (
@@ -21,10 +34,35 @@ CREATE TABLE ksms.payment_type (
 );
 
 CREATE TABLE ksms.payment (
-	person_id bigserial PRIMARY KEY,
+	person_id bigserial PRIMARY KEY REFERENCES ksms.person (id),
 	amount numeric(10,2) NOT NULL,
 	payment_date timestamp with time zone NOT NULL,
-	payment_type_id bigserial,
- 	FOREIGN KEY (person_id) REFERENCES ksms.person (id),
-	FOREIGN KEY (payment_type_id) REFERENCES ksms.payment_type (id)
+	payment_type_id bigserial REFERENCES ksms.payment_type (id)
 );
+
+CREATE TABLE ksms.grading (
+	id bigserial PRIMARY KEY,
+	grading_date timestamp with time zone NOT NULL,
+	cost numeric(10,2) NOT NULL
+);
+
+CREATE TABLE ksms.belt_attempt (
+	student_id bigserial REFERENCES ksms.student (person_id),
+	grading_id bigserial REFERENCES ksms.grading (id),
+	belt_id bigserial,
+	pass boolean NOT NULL,
+	PRIMARY KEY (student_id, grading_id)
+);
+
+INSERT INTO ksms.payment_type (description) VALUES
+	('VISA'),
+	('MASTERCARD'),
+	('PAYPAL'),
+	('CASH');
+
+INSERT INTO ksms.belt (description) VALUES
+	('WHITE'),
+	('YELLOW'),
+	('BLUE'),
+	('RED'),
+	('BLACK');
--- src/ksms.php
+++ src/ksms.php
@@ -15,8 +15,7 @@ class DojoRepo {
 	}
 
 	public function findStudentById($id) {
-		$student = new Student();
-		return $student;
+		$student = new Student(); return $student;
 	}
 
 	public function findStudentByEmail($email_address) {
@@ -27,12 +26,43 @@ class DojoRepo {
 	public function updateStudentInfo(Student $s) {
 		echo "UPDATE person SEt name=:name, street=:street, town=:town, phone=:phone, email_address=:email_address\n";
 	}
+
 	public function dropStudent(Student $s) {
 		echo "UPDATE student SET drop_date=NOW() WHERE id = :student_id\n";
 	}
+
 	public function acceptPayment(Student $s, $amount, $payment_type) {
-		echo "INSERT INTO payment (amount, payment_type_id, person_id) VALUES (:amount, :payment_type_id, person_id)\n";
+		echo "INSERT INTO payment (amount, payment_type_id, person_id) ( SELECT :amount, id, :person_id FROM payment_type WHERE description = :payment_type))\n";
 	}
+
+	public function promoteToHigherBelt(Student $s, $belt) {
+		echo "UPDATE ksms.student SET belt_id=(SELECT FROM ksms.belt WHERE description=:belt)\n";
+	}
+
+	public function inviteForGrading(Student $s, $grading_date) {
+		echo "UPDATE student SET grading_date = :grading_date\n";
+	}
+
+	public function emailMembership(Student $s) {
+		$membership_info = $this->getMembershipInfo($s);
+		$email_addr = $s->email_address;
+		$this->dispatchEmail($email_addr, $membership_info);
+	}
+
+	public function printMembership(Student $s) {
+		$membership_info = $this->getMembershipInfo($s);
+		$this->printer($membership_info);
+	}
+
+	public function getMembershipInfo() {
+		return array(
+			'name' => 'Name',
+			'belt' => 'Belt',
+			'enroll_date' => 'Enroll Date');
+	}
+
+	public function dispatchEmail($email, $membership_info) { echo "sending email\n"; }
+	public function printer($membership_info) { echo "printing membership info\n"; }
 }
 
 class Dojo {
@@ -46,8 +76,22 @@ class Dojo {
 	public function acceptPayment(Student $s, $amount, $payment_type) { 
 		$this->dojo_repo->acceptPayment($s, $amount, $payment_type); 
 	}
+	public function promoteToHigherBelt(Student $s, $belt) {
+		$this->dojo_repo->promoteToHigherBelt($s, $belt);
+	}
+	public function inviteForGrading(Student $s, $date) {
+		$this->dojo_repo->inviteForGrading($s, $date);
+	}
+	public function emailMembership(Student $s) {
+		$this->dojo_repo->emailMembership($s);
+	}
+	public function printMembership(Student $s) {
+		$this->dojo_repo->printMembership($s);
+	}
+	public function scheduleGrading(Student $s) { }
+	public function printCertificate(Student $s) { }
+	public function holdMembership(Student $s) { }
 }
-
 
 $john = new Student();
 $john->name = "John Smith";
@@ -62,3 +106,7 @@ var_dump($dojo->findStudentById(1));
 var_dump($dojo->findStudentByEmail("john@test.net"));
 $dojo->dropStudent($john);
 $dojo->acceptPayment($john, 30.00, 'VISA');
+$dojo->promoteToHigherBelt($john, 'RED');
+$dojo->emailMembership($john);
+$dojo->printMembership($john);
+$dojo->inviteForGrading($john, '12-12-2016');
